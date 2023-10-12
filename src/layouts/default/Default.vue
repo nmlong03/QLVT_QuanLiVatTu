e3ba7a46199e5703c8ec707cce7a0360a5aa9323
Thanh202 <lethanh27112002@gmail.com>
2023-10-12 16:35:47 +0700

    fix: siderbar

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,100 +1,119 @@
 <template>
   <v-container>
     <v-layout>
-      <v-card min-width="100%">
+      <v-card min-width="100%"  variant="text" >
+
         <!-- sidebar  -->
         <v-navigation-drawer
-          color="blue"
+          v-model="drawer"
+          color="#232D3F"
           class="px-0"
           permanent
-          rail
-          expand-on-hover
+          :rail="rail"
           ><v-list-item
             prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
             title="Administrator"
-            nav
           ></v-list-item>
           <v-divider></v-divider>
           <v-list nav class="text-start">
-            <v-list-item
-              prepend-icon="mdi-home"
+            <router-link to="/loai-vat-tu">
+              <v-list-item
+              prepend-icon="mdi-archive"
               @click="console.log('clicked')"
             >
-              <v-list-item-title>List 1</v-list-item-title>
+              <v-list-item-title style="margin-left: 10px;font-weight: bold;">Loại vật tư</v-list-item-title>
             </v-list-item>
-            <v-list-group>
+            </router-link>
+
+            <v-list-group v-for="(item, index) of MENU_NAVIGATION" :key="index">
               <template v-slot:activator="{ props }">
-                <v-list-item prepend-icon="mdi-home-city" v-bind="props">
-                  <v-list-item-title>Test 1</v-list-item-title>
+                <v-list-item v-bind="props">
+                  <v-list style="display: flex; align-items: center">
+                    <v-icon :icon="item.icon"></v-icon>
+                    <v-list-item-title style="margin-left: 40px;     font-weight: bold;" >
+                    {{item.dropText}}
+                    </v-list-item-title>
+                  </v-list>
                 </v-list-item>
               </template>
-              <v-list-item @click="console.log('clicked')">test1</v-list-item>
-              <v-list-item @click="console.log('clicked')">test2</v-list-item>
+              <v-list v-if="item.children">
+                <v-list v-for="(i, index) of item.children" :key="index">
+                  <router-link :to="i.path">
+                    <v-list-item @click="console.log('clicked')" style="font-size: 12px">{{ i.title }}</v-list-item>
+                  </router-link>
+              </v-list>
+              </v-list>
             </v-list-group>
-            <v-list-group>
-              <template v-slot:activator="{ props }">
-                <v-list-item prepend-icon="mdi-account" v-bind="props">
-                  <v-list-item-title>Test 2</v-list-item-title>
-                </v-list-item>
-              </template>
-              <v-list-item @click="console.log('clicked')">test1</v-list-item>
-              <v-list-item @click="console.log('clicked')">test2</v-list-item>
-              <v-list-item @click="console.log('clicked')">test3</v-list-item>
-            </v-list-group>
-            <v-list-item
-              prepend-icon="mdi-home"
-              @click="console.log('clicked')"
-            >
-              <v-list-item-title>List 2</v-list-item-title>
-            </v-list-item>
           </v-list>
           <template v-slot:append>
             <div class="pa-0">
               <v-list-item
-                prepend-icon="mdi-home"
+                prepend-icon="mdi-logout"
                 @click="console.log('clicked')"
               >
-                <v-list-item-title>Logout</v-list-item-title>
+                <v-list-item-title style="margin-right:40px">Đăng Xuất</v-list-item-title>
               </v-list-item>
             </div>
           </template>
         </v-navigation-drawer>
+
         <!-- navbar  -->
-        <v-app-bar color="gray">
+        <v-container fluid="true" style="margin-top: 40px;border-style: none">
+          <v-app-bar  color="gray">
           <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
-          <v-app-bar-title>Application</v-app-bar-title>
+          <v-app-bar-title style="margin-right:200px" >Application Bar</v-app-bar-title>
           <v-spacer></v-spacer>
-          <v-icon class="mr-4">mdi-magnify</v-icon>
-          <v-icon class="mr-4">mdi-heart</v-icon>
+          <v-text-field
+            density="compact"
+            variant="solo"
+            label="Search"
+            append-inner-icon="mdi-magnify"
+            single-line
+            hide-details
+            flat
+    >      </v-text-field>
+          <v-icon class="mr-4">mdi-bell</v-icon>
           <v-icon>mdi-dots-vertical</v-icon>
+
         </v-app-bar>
+          <v-app overlaps="" style="width: 100%; height: 100vh;" flax>
+            <router-view></router-view>
+          </v-app>
+
+        </v-container>
+
+
       </v-card>
 
-      <router-view />
     </v-layout>
+
   </v-container>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import { MENU_NAVIGATION } from "@/navigation/vertical";
+console.log(MENU_NAVIGATION);
+
 const drawer = ref(false);
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Arial, Helvetica, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 nav {
-  padding: 30px;
+  padding: 15px;
   a {
-    font-weight: bold;
-    color: #2c3e50;
+    text-decoration: none;
+    color: #ffffff;
     &.router-link-exact-active {
       color: #42b983;
+
     }
   }
 }
--- src/navigation/vertical.js
+++ src/navigation/vertical.js
@@ -1,18 +1,45 @@
 export const MENU_NAVIGATION = [
   {
-    dropText: "long",
+    dropText: "Chi tiết vật tư",
+    icon: "mdi-archive-search",
     children: [
-      { name: "l1", title: "l1" },
-      { name: "l1", title: "l1" },
+      { title: "Danh sách vật tư", path: "/danh-sach-vat-tu" },
+      { title: "Thêm thông tin vật tư", path: "/thong-tin-vat-tu" },
     ],
   },
   {
-    dropText: "long2",
+    dropText: "Khách hàng",
+    icon: "mdi-account-file-text",
     children: [
       {
-        name: "l2",
-        title: "l2",
+        title: "Danh sách khách hàng đã thuê",
+        path: "/danh-sach-khach-hang-thue",
       },
+    ],
+  },
+  {
+    dropText: "Hóa Đơn",
+    icon: "mdi-application-outline",
+    children: [
+      { title: "Xuất hóa đơn", path: "/xuat-hoa-don" },
+      { title: "Thống kê hóa đơn", path: "/thong-ke-hoa-don" },
+    ],
+  },
+  {
+    dropText: "Lịch sử giao dịch",
+    icon: "mdi-book-open-outline",
+    children: [
+      {
+        title: "Danh sách lịch sử giao dịch",
+        path: "/danh-sach-lich-su-giao-dich",
+      },
+    ],
+  },
+  {
+    dropText: "Thống kê",
+    icon: "mdi-alarm-panel",
+    children: [
+      { title: "Thống kê doanh thu", path: "/thong-ke-doanh-thu" },
     ],
   },
 ];
