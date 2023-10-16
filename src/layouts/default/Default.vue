df8a1775f7cae67926470839b35dc6faefdd57b1
Hang0706 <nhang1396@gmail.com>
2023-10-16 10:03:29 +0700

    delete comment in login.vue

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,28 +1,17 @@
 <template>
   <v-container>
     <v-layout>
-      <v-card min-width="100%"  variant="text" >
+      <v-card min-width="100%" variant="text">
 
         <!-- sidebar  -->
-        <v-navigation-drawer
-          v-model="drawer"
-          color="#232D3F"
-          class="px-0"
-          permanent
-          :rail="rail"
-          ><v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-            title="Administrator"
-          ></v-list-item>
+        <v-navigation-drawer v-model="drawer" color="#232D3F" class="px-0" permanent :rail="rail"><v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="Administrator"></v-list-item>
           <v-divider></v-divider>
           <v-list nav class="text-start">
             <router-link to="/loai-vat-tu">
-              <v-list-item
-              prepend-icon="mdi-archive"
-              @click="console.log('clicked')"
-            >
-              <v-list-item-title style="margin-left: 10px;font-weight: bold;">Loại vật tư</v-list-item-title>
-            </v-list-item>
+              <v-list-item prepend-icon="mdi-archive" @click="console.log('clicked')">
+                <v-list-item-title style="margin-left: 10px;font-weight: bold;">Loại vật tư</v-list-item-title>
+              </v-list-item>
             </router-link>
 
             <v-list-group v-for="(item, index) of MENU_NAVIGATION" :key="index">
@@ -30,8 +19,8 @@
                 <v-list-item v-bind="props">
                   <v-list style="display: flex; align-items: center">
                     <v-icon :icon="item.icon"></v-icon>
-                    <v-list-item-title style="margin-left: 40px;     font-weight: bold;" >
-                    {{item.dropText}}
+                    <v-list-item-title style="margin-left: 40px;     font-weight: bold;">
+                      {{ item.dropText }}
                     </v-list-item-title>
                   </v-list>
                 </v-list-item>
@@ -41,16 +30,13 @@
                   <router-link :to="i.path">
                     <v-list-item @click="console.log('clicked')" style="font-size: 12px">{{ i.title }}</v-list-item>
                   </router-link>
-              </v-list>
+                </v-list>
               </v-list>
             </v-list-group>
           </v-list>
           <template v-slot:append>
             <div class="pa-0">
-              <v-list-item
-                prepend-icon="mdi-logout"
-                @click="console.log('clicked')"
-              >
+              <v-list-item prepend-icon="mdi-logout" @click="console.log('clicked')">
                 <v-list-item-title style="margin-right:40px">Đăng Xuất</v-list-item-title>
               </v-list-item>
             </div>
@@ -59,23 +45,16 @@
 
         <!-- navbar  -->
         <v-container fluid="true" style="margin-top: 40px;border-style: none">
-          <v-app-bar  color="gray">
-          <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
-          <v-app-bar-title style="margin-right:200px" >Application Bar</v-app-bar-title>
-          <v-spacer></v-spacer>
-          <v-text-field
-            density="compact"
-            variant="small"
-            label="Search"
-            append-inner-icon="mdi-magnify"
-            single-line
-            hide-details
-            flat
-    >      </v-text-field>
-          <v-icon class="mr-4">mdi-bell</v-icon>
-          <v-icon>mdi-dots-vertical</v-icon>
+          <v-app-bar color="gray">
+            <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
+            <v-app-bar-title style="margin-right:200px">Application Bar</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <v-text-field density="compact" variant="small" label="Search" append-inner-icon="mdi-magnify" single-line
+              hide-details flat> </v-text-field>
+            <v-icon class="mr-4">mdi-bell</v-icon>
+            <v-icon>mdi-dots-vertical</v-icon>
 
-        </v-app-bar>
+          </v-app-bar>
 
 
         </v-container>
@@ -106,11 +85,14 @@ const drawer = ref(false);
   text-align: center;
   color: #2c3e50;
 }
+
 nav {
   padding: 15px;
+
   a {
     text-decoration: none;
     color: #ffffff;
+
     &.router-link-exact-active {
       color: #42b983;
 
